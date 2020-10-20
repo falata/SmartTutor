31eb57424f25533fef10b042e01d2f7db33e210f
rmogusu <rmogusu123@gmail.com>
2020-10-20 19:09:28 +0300

    edit

--- app/src/main/java/com/example/smarttutor/MainActivity.java
+++ app/src/main/java/com/example/smarttutor/MainActivity.java
@@ -7,23 +7,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button findTutor;
-    Button tutor;
-    Context mContext = MainActivity.this;
+    private Button mFindTutorsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findTutor = findViewById(R.id.looking_for_a_tutor_button);
-        tutor = findViewById(R.id.i_am_a_tutor_button);
-
-        findTutor.setOnClickListener(new View.OnClickListener() {
+        mFindTutorsButton = (Button) findViewById(R.id.findTutorsButton);
+        mFindTutorsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(mContext, TutorActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
             }
         });
     }
--- app/src/main/java/com/example/smarttutor/TutorActivity.java
+++ app/src/main/java/com/example/smarttutor/TutorActivity.java
@@ -1,39 +1,17 @@
 package com.example.smarttutor;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-import com.example.smarttutor.model.TutorInfo;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class TutorActivity extends AppCompatActivity {
 
-    public TutorInfoAdapter mAdapter;
-    private DataViewModel mDataViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor);
-        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        final LinearLayoutManager notesLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(notesLayoutManager);
-        mAdapter = new TutorInfoAdapter(this);
-        recyclerView.setAdapter(mAdapter);
-        mDataViewModel= new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(DataViewModel.class);
-        mDataViewModel.tutorInfoMutableData.observe(this, new Observer<List<TutorInfo>>() {
-            @Override
-            public void onChanged(List<TutorInfo> tutorInfos) {
-                mAdapter.setList(tutorInfos);
-            }
-        });
+
 
     }
 }
